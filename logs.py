d8f842f0efcbc057848a3c72f9b81455fe171992
Matthias Rampke <matthias@2pktfkt.de>
2010-01-27 18:42:11 +0100

    + header, footer, pattern substitution system

--- logs.py
+++ logs.py
@@ -1,12 +1,27 @@
 from mod_python import apache
+import re
 
 content_type = "text/html; charset=UTF8"
+head = """<html>
+<body>"""
+foot = """</body>
+</html>"""
+
+patterns = [
+    (r"^(\d\d):(\d\d) (.*)$", r'<a href="#\1\2" name="\1\2">\1:\2</a> \3'),
+    (r'^(.*)$',r'<p class="line">\1</p>'),
+    ]
+
+patterns_compiled = [(re.compile(p,re.U),s) for p,s in patterns]
 
 def handler(req):
   req.content_type = content_type
   f = open(req.filename)
-  req.write("<html><body>")
+  req.write(head)
   for l in f:
-    req.write("<p>" + l + "</p>")
-  req.write("</body></html>")
+    for p,s in patterns_compiled:
+      l = p.sub(s,l)
+    req.write(l)
+  req.write(foot)
   f.close()
+  return apache.OK
